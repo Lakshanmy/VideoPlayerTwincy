b9b3ada95e4ee2c7010b0ac4b0392924099ac318
Lakshan <78016063+Lakshanmy@users.noreply.github.com>
2021-09-15 12:56:50 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo...."
-git clone https://github.com/subinps/VCPlayerBot /VCPlayerBot
-cd /VCPlayerBot
+git clone https://github.com/lakshanmy/VideoPlayerTwincy
+cd /VideoPlayerTwincy
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 main.py
